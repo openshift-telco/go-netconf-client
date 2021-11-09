c9ec76ee59580315f54ed573287bb6db0f381ad5
Alexis de Talhouët <adetalhouet89@gmail.com>
2021-11-09 15:32:38 -0500

    At
    Signed-off-by: Alexis de Talhouët <adetalhouet89@gmail.com>

--- netconf/transport.go
+++ netconf/transport.go
@@ -1,87 +1,96 @@
+// Go NETCONF Client
+//
+// Copyright (c) 2013-2018, Juniper Networks, Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package netconf
 
 import (
-	"bufio"
 	"bytes"
 	"encoding/xml"
-	"errors"
 	"fmt"
 	"github.com/adetalhouet/go-netconf/netconf/message"
 	"io"
-	"strconv"
+	"regexp"
 )
 
 const (
-	msgSeparator    = "]]>]]>"
-	msgSeparatorV11 = "\n##\n"
+	// msgSeperator is used to separate sent messages via NETCONF
+	msgSeperator     = "]]>]]>"
+	msgSeperator_v11 = "\n##\n"
 )
 
-// Transport is the underlying communication medium toward a NETCONF server.
-// It is responsible for sending and receiving messages through the wire.
+// DefaultCapabilities sets the default capabilities of the client library
+//var DefaultCapabilities = []string{
+//	"urn:ietf:params:netconf:base:1.0",
+//	"urn:ietf:params:netconf:base:1.1",
+//}
+
+// HelloMessage is used when bringing up a NETCONF session
+//type HelloMessage struct {
+//	XMLName      xml.Name `xml:"urn:ietf:params:xml:ns:netconf:base:1.0 hello"`
+//	Capabilities []string `xml:"capabilities>capability"`
+//	SessionID    int      `xml:"session-id,omitempty"`
+//}
+
+// Transport interface defines what characterisitics make up a NETCONF transport
+// layer object.
 type Transport interface {
 	Send([]byte) error
 	Receive() ([]byte, error)
 	Close() error
 	ReceiveHello() (*message.Hello, error)
-	SendHello(*message.Hello) error
+	SendHello(hello *message.Hello) error
 	SetVersion(version string)
 }
 
-type transportIO struct {
+type transportBasicIO struct {
 	io.ReadWriteCloser
+	//new add
 	version string
 }
 
-// SetVersion sets the underlying supported NETCONF protocol version.
-func (t *transportIO) SetVersion(version string) {
+func (t *transportBasicIO) SetVersion(version string) {
 	t.version = version
 }
 
-// Send formats provided byte into a NETCONF specific message, by surrounding it with appropriate separator.
-func (t *transportIO) Send(data []byte) error {
-	var separator []byte
+// Sends a well formated NETCONF rpc message as a slice of bytes adding on the
+// nessisary framining messages.
+func (t *transportBasicIO) Send(data []byte) error {
+	var seperator []byte
 	var dataInfo []byte
-
+	//headlen := 0
 	if t.version == "v1.1" {
-		separator = append(separator, []byte(msgSeparatorV11)...)
-		header := fmt.Sprintf("\n#%d\n", len(string(data)))
-		dataInfo = append(dataInfo, header...)
+		seperator = append(seperator, []byte(msgSeperator_v11)...)
 	} else {
-		separator = append(separator, []byte(msgSeparator)...)
+		seperator = append(seperator, []byte(msgSeperator)...)
 	}
 
+	if t.version == "v1.1" {
+		header := fmt.Sprintf("\n#%d\n", len(string(data)))
+		dataInfo = append(dataInfo, header...)
+		//t.Write([]byte(header))
+		//headlen = len([]byte(header))
+	}
 	dataInfo = append(dataInfo, data...)
-	dataInfo = append(dataInfo, separator...)
-
-	println(string(dataInfo))
-
+	dataInfo = append(dataInfo, seperator...)
 	_, err := t.Write(dataInfo)
 
 	return err
 }
 
-// Receive reads the byte and if we have a chunked message, properly construct it.
-func (t *transportIO) Receive() ([]byte, error) {
-	var separator []byte
+func (t *transportBasicIO) Receive() ([]byte, error) {
+	var seperator []byte
 	if t.version == "v1.1" {
-		separator = append(separator, []byte(msgSeparatorV11)...)
-		// NOTES: This is not clever at all
-		// you are reading the O-RU response content once with WaitForBytes, and then you read it again to get rid of
-		// the #<chunk-size> pieces. Using Chunked would be enough, but if you pass in the t.ReadWriteCloser to the
-		// splitChunked function it gets stuck when doing the Read of the last piece of the NETCONF message.
-		// This will need to be addressed in the future.
-		b, err := t.WaitForBytes(separator)
-		if err != nil {
-			return nil, err
-		}
-		return t.Chunked(b)
+		seperator = append(seperator, []byte(msgSeperator_v11)...)
 	} else {
-		separator = append(separator, []byte(msgSeparator)...)
-		return t.WaitForBytes(separator)
+		seperator = append(seperator, []byte(msgSeperator)...)
 	}
+	return t.WaitForBytes([]byte(seperator))
 }
 
-func (t *transportIO) SendHello(hello *message.Hello) error {
+func (t *transportBasicIO) SendHello(hello *message.Hello) error {
 	val, err := xml.Marshal(hello)
 	if err != nil {
 		return err
@@ -89,12 +98,11 @@ func (t *transportIO) SendHello(hello *message.Hello) error {
 
 	header := []byte(xml.Header)
 	val = append(header, val...)
-
 	err = t.Send(val)
 	return err
 }
 
-func (t *transportIO) ReceiveHello() (*message.Hello, error) {
+func (t *transportBasicIO) ReceiveHello() (*message.Hello, error) {
 	hello := new(message.Hello)
 
 	val, err := t.Receive()
@@ -106,123 +114,13 @@ func (t *transportIO) ReceiveHello() (*message.Hello, error) {
 	return hello, err
 }
 
-// SplitChunked returns a bufio.SplitFunc suitable for decoding
-// "chunked framing" NETCONF transport streams.
-//
-// endOfMessage will be called at the end of each NETCONF message,
-// and must not be nil.
-//
-// It must only be used with bufio.Scanner who have a buffer of
-// at least 16 bytes (rarely is this a concern).
-//
-// splitChunked function comes from the andaru/netconf library
-func SplitChunked(endOfMessage func()) bufio.SplitFunc {
-	type stateT int
-	const (
-		headerStart stateT = iota
-		headerSize
-		data
-		endOfChunks
-	)
-	var state stateT
-	var cs, dataleft int
-
-	return func(b []byte, atEOF bool) (advance int, token []byte, err error) {
-		for cur := b[advance:]; err == nil && advance < len(b); cur = b[advance:] {
-			if len(cur) < 4 && !atEOF {
-				return
-			}
-			switch state {
-			case headerStart:
-				switch {
-				case bytes.HasPrefix(cur, []byte("\n#")):
-					if len(cur) < 4 {
-						err = ErrBadChunk
-						return
-					}
-					switch r := cur[2]; {
-					case r == '#':
-						advance += 3
-						state = endOfChunks
-					case r >= '1' && r <= '9':
-						advance += 2
-						state = headerSize
-					default:
-						err = ErrBadChunk
-					}
-				default:
-					err = ErrBadChunk
-				}
-			case headerSize:
-				switch idx := bytes.IndexByte(cur, '\n'); {
-				case idx < 1, idx > 10:
-					if len(cur) < 11 && !atEOF {
-						return
-					}
-					err = ErrBadChunk
-				default:
-					csize := cur[:idx]
-					if csizeVal, csizeErr := strconv.ParseUint(string(csize), 10, 31); csizeErr != nil {
-						err = ErrBadChunk
-					} else {
-						advance += idx + 1
-						dataleft = int(csizeVal)
-						state = data
-					}
-				}
-			case data:
-				var rsize int
-				if rsize = len(cur); dataleft < rsize {
-					rsize = dataleft
-				}
-				token = append(token, cur[:rsize]...)
-				advance += rsize
-				if dataleft -= rsize; dataleft < 1 {
-					state = headerStart
-					cs++
-				}
-				if rsize > 0 {
-					return
-				}
-			case endOfChunks:
-				switch r := cur[0]; {
-				case r == '\n' && cs > 0:
-					advance++
-					state = headerStart
-					if endOfMessage != nil {
-						endOfMessage()
-					}
-				default:
-					err = ErrBadChunk
-				}
-			}
-		}
-		if atEOF && dataleft > 0 {
-			//state = headerStart
-			err = io.ErrUnexpectedEOF
-		}
-		return
-	}
+func (t *transportBasicIO) Writeln(b []byte) (int, error) {
+	t.Write(b)
+	t.Write([]byte("\n"))
+	return 0, nil
 }
 
-// ErrBadChunk indicates a chunked framing protocol error occurred
-var ErrBadChunk = errors.New("bad chunk")
-
-func (t *transportIO) Chunked(b []byte) ([]byte, error) {
-	rdr := bytes.NewReader(b)
-	scanner := bufio.NewScanner(rdr)
-	bsize := 16
-	scanner.Buffer(make([]byte, bsize), bsize*2)
-
-	scanner.Split(SplitChunked(nil))
-	var got []byte
-	for scanner.Scan() {
-		got = append(got, scanner.Bytes()...)
-	}
-	return got, nil
-}
-
-func (t *transportIO) WaitForFunc(f func([]byte) (int, error)) ([]byte, error) {
+func (t *transportBasicIO) WaitForFunc(f func([]byte) (int, error)) ([]byte, error) {
 	var out bytes.Buffer
 	buf := make([]byte, 8192)
 
@@ -231,7 +129,6 @@ func (t *transportIO) WaitForFunc(f func([]byte) (int, error)) ([]byte, error) {
 		n, err := t.Read(buf[pos : pos+(len(buf)/2)])
 		if err != nil {
 			if err != io.EOF {
-				fmt.Println(fmt.Sprintf("Failed here 1: /n%s", err))
 				return nil, err
 			}
 			break
@@ -240,7 +137,6 @@ func (t *transportIO) WaitForFunc(f func([]byte) (int, error)) ([]byte, error) {
 		if n > 0 {
 			end, err := f(buf[0 : pos+n])
 			if err != nil {
-				fmt.Println(fmt.Sprintf("Failed here 2: /n%s", err))
 				return nil, err
 			}
 
@@ -258,15 +154,36 @@ func (t *transportIO) WaitForFunc(f func([]byte) (int, error)) ([]byte, error) {
 		}
 	}
 
-	return nil, fmt.Errorf("WaitForFunc failed. %s", out.Bytes())
+	return nil, fmt.Errorf("WaitForFunc failed")
 }
 
-func (t *transportIO) WaitForBytes(b []byte) ([]byte, error) {
-	return t.WaitForFunc(
-		func(buf []byte) (int, error) {
-			return bytes.Index(buf, b), nil
-		},
-	)
+func (t *transportBasicIO) WaitForBytes(b []byte) ([]byte, error) {
+	return t.WaitForFunc(func(buf []byte) (int, error) {
+		return bytes.Index(buf, b), nil
+	})
+}
+
+func (t *transportBasicIO) WaitForString(s string) (string, error) {
+	out, err := t.WaitForBytes([]byte(s))
+	if out != nil {
+		return string(out), err
+	}
+	return "", err
+}
+
+func (t *transportBasicIO) WaitForRegexp(re *regexp.Regexp) ([]byte, [][]byte, error) {
+	var matches [][]byte
+	out, err := t.WaitForFunc(func(buf []byte) (int, error) {
+		loc := re.FindSubmatchIndex(buf)
+		if loc != nil {
+			for i := 2; i < len(loc); i += 2 {
+				matches = append(matches, buf[loc[i]:loc[i+1]])
+			}
+			return loc[1], nil
+		}
+		return -1, nil
+	})
+	return out, matches, err
 }
 
 // ReadWriteCloser represents a combined IO Reader and WriteCloser
--- netconf/transport_ssh.go
+++ netconf/transport_ssh.go
@@ -31,7 +31,7 @@ const (
 // TransportSSH maintains the information necessary to communicate with the
 // remote device over SSH
 type TransportSSH struct {
-	transportIO
+	transportBasicIO
 	sshClient  *ssh.Client
 	sshSession *ssh.Session
 }
