c045f23028f87e9377efbb153072163215080500
Alexis de Talhouët <adetalhouet89@gmail.com>
2021-11-14 16:24:12 -0500

    Rework examples
    Signed-off-by: Alexis de Talhouët <adetalhouet89@gmail.com>

---
+++ examples/test-notification.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"github.com/adetalhouet/go-netconf/netconf"
+	"github.com/adetalhouet/go-netconf/netconf/message"
+	"golang.org/x/crypto/ssh"
+	"log"
+)
+
+func main() {
+	sshConfig := &ssh.ClientConfig{
+		User:            "lab",
+		Auth:            []ssh.AuthMethod{ssh.Password("lab")},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+
+	s, err := netconf.DialSSH("10.64.1.54:32035", sshConfig)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer s.Close()
+
+	fmt.Println(s.Capabilities)
+	fmt.Println(s.SessionID)
+
+	capabilities := netconf.DefaultCapabilities
+	capabilities = append(capabilities, "urn:ietf:params:xml:ns:yang:ietf-event-notifications")
+	s.SendHello(&message.Hello{Capabilities: capabilities})
+
+	d := "<establish-subscription\n        xmlns=\"urn:ietf:params:xml:ns:yang:ietf-event-notifications\"\n        xmlns:yp=\"urn:ietf:params:xml:ns:yang:ietf-yang-push\">\n      <stream>yp:yang-push</stream>\n      <yp:xpath-filter>/mdt-oper:mdt-oper-data/mdt-subscriptions</yp:xpath-filter>\n      <yp:period>1000</yp:period>\n    </establish-subscription>"
+	handleReply(s.ExecRPC(message.NewRPC(d)))
+
+	condition := false
+	counter := 5
+	for ok := true; ok; ok = !condition {
+		rawXML, err := s.Transport.Receive()
+		if err != nil {
+			panic(err)
+		}
+
+		var rawReply = string(rawXML)
+		fmt.Printf("%+v", rawReply)
+
+		counter--
+
+		if counter == 0 {
+			condition = true
+		}
+	}
+}
